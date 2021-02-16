08a96daa30275b219d4f0c646297dace56675097
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-02-16 15:54:00 -0600

    Added pseudo-code to write out the logical steps needed to complete this project.

--- script.js
+++ script.js
@@ -15,11 +15,6 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-
-
 // ****** My Attempt ******
 
 
@@ -34,12 +29,21 @@ console.log(passwordLength, wantLowercase, wantUppercase, wantNumbers, wantSpeci
 
 
 
+
+// Based on the length and true/false values of the character types they want, I need to spit out a random password.
+
+/* One array for "Characters" and sub-arrays inside it maybe, with all the possible character types? 
+  and then if the user choose 10 characters and True for all, then a random number function that iterates through the qualifying sub-arrays?
+/*
+
+
+
 // Generator Functions
 
 /* Multiply a random decimal (btwn 0-1) times 26 (numbers of letters in alphabet). 
-    Then adds 97 to itto put the result between 97 - 123 (all lowercase chracters in the Browser charset) */
+    Then adds 97 to input the result between 97 - 123 (range of lowercase characters in the Browser charset) */
 function randomLower () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function randomUpper () {
@@ -59,10 +63,20 @@ console.log(randomNumber());
 console.log(randomSymbol());
 
 
+// loops through all of the 'random' generator funnctions X amount of times, based on the password length chosen by user
+function generateTest (testLength) {
+  for (var i = 0; i <= testLength; i++) {
+    
+  }
+}
 
 
 
-// Can do the same thing with a set of arrays instead of using charset. Lowercase example:
+
+
+
+
+// Can do also randomly generate characters with a set of arrays instead of using charset. Lowercase example:
 
 var lowercaseArr = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
 var lowerChoice;
